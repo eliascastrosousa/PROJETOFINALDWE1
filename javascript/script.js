11db2b35cb290793ee7e9d6f943fbe0caf614fef
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-07-02 12:44:24 -0300

    commit atualizações

--- javascript/script.js
+++ javascript/script.js
@@ -1,27 +1,12 @@
-var nome = document.getElementsByClassName('nome'.toUpperCase)
-var qtd = document.getElementsByClassName('qtd')
-var quantidade = Number(qtd.value)
-var msg = document.getElementById('msg')
+const obj_sel_rede_social = document.querySelector('#sel_rede_social') // resgatando o elemento pelo id
+const obj_img_rede_social = document.querySelector('#img_rede_social')
+const obj_p_rede_social = document.querySelector('#p_rede_social')
 
-function calcular() {
+obj_sel_rede_social.addEventListener('change', FunMudaFigura) // adcionando o evento escolha e dando o nome de FunMudaFigura
 
-    if (nome = 'BMW') {
-        var taxa = 200
-        msg.innerHTML = `O valor para o aluguel da BMW durante ${quantidade} Horas será de  R$${taxa * quantidade},00.`
-    }
-    else if (nome = 'AUDI') {
-        var taxa = 250
-        msg.innerHTML = `O valor para o aluguel do Audi G3 durante ${quantidade} Horas será de  R$${taxa * quantidade},00.`
-    }
-    else if (nome = 'MERCEDES') {
-        var taxa = 280
-        msg.innerHTML = `O valor para o aluguel da Mercedes durante ${quantidade} Horas será de  R$${taxa * quantidade},00.`
-    }
-    else if (nome = 'HYUNDAI') {
-        var taxa = 170
-        msg.innerHTML = `O valor para o aluguel da Hyundai durante ${quantidade} Horas será de  R$${taxa * quantidade},00.`
-    }
-    else {
-        alert('Valores inconcistentes')
-    }
+function FunMudaFigura() { // criando a função a partir da change 
+    obj_img_rede_social.setAttribute('src', 'imagens/' + obj_sel_rede_social.value + '.jpg') // adcionando a linha src="" ao objeto 
+    obj_img_rede_social.setAttribute('alt', 'Logo ' + obj_sel_rede_social.value) 
+    obj_img_rede_social.setAttribute('title', 'Logo ' + obj_sel_rede_social.value)
+    obj_p_rede_social.innerText = 'Logo ' + obj_sel_rede_social.value // adcionando o texto ao paragrafo vazio
 }
