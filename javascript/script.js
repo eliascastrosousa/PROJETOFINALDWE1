736ada3cb78290e800cbcda793365d32b68f418f
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2021-07-19 16:55:09 -0300

    commit

--- javascript/script.js
+++ javascript/script.js
@@ -16,31 +16,44 @@ function FunMudaFigura() { // criando a função a partir da change
         obj_p_detalhes.innerText = 'Modelo com travas e vidros eletricos. '
         obj_p_detalhes.innerHTML += `Deseja Alugar? <a href="#alugueaqui"> Clique aqui!</a>`
 
-        
-
     }else if (sel_carro.value == 'Ford Fiesta'){
         obj_h2texto.innerHTML = `${sel_carro.value} Ano 2019` 
         obj_h3texto.innerText = 'Aluguel: R$140,00 Diaria' 
         obj_p_detalhes.innerText = 'Modelo completo com Ar condicionado, travas e vidros eletricos. '
+        obj_p_detalhes.innerHTML += `Deseja Alugar? <a href="#alugueaqui"> Clique aqui!</a>`
 
     }else if (sel_carro.value == 'VolksWagen Gol'){
         obj_h2texto.innerHTML = `${sel_carro.value} Ano 2020` 
         obj_h3texto.innerText = 'Aluguel: R$130,00 Diaria' 
         obj_p_detalhes.innerText = 'Modelo completo com Ar condicionado, travas e vidros eletricos. '
+        obj_p_detalhes.innerHTML += `Deseja Alugar? <a href="#alugueaqui"> Clique aqui!</a>`
 
     }else if (sel_carro.value == 'Fiat Novo Uno'){
         obj_h2texto.innerHTML = `${sel_carro.value} Ano 2018 <br>` 
         obj_h3texto.innerText = 'Aluguel: R$100,00 Diaria' 
         obj_p_detalhes.innerText = 'Modelo completo travas e vidros eletricos. '
+        obj_p_detalhes.innerHTML += `Deseja Alugar? <a href="#alugueaqui"> Clique aqui!</a>`
 
     }else if (sel_carro.value == 'Chevrolet Onix'){
         obj_h2texto.innerHTML = `${sel_carro.value} Ano 2020` 
         obj_h3texto.innerText = 'Aluguel: R$130,00 Diaria' 
         obj_p_detalhes.innerText = 'Modelo completo com Ar condicionado, travas e vidros eletricos. '
+        obj_p_detalhes.innerHTML += `Deseja Alugar? <a href="#alugueaqui"> Clique aqui!</a>`
 
     }else if (sel_carro.value == 'Renaul Kwid'){
         obj_h2texto.innerHTML = `${sel_carro.value} Ano 2021` 
         obj_h3texto.innerText = 'Aluguel: R$120,00 Diaria' 
         obj_p_detalhes.innerText = 'Modelo completo com Ar condicionado, travas e vidros eletricos. '
+        obj_p_detalhes.innerHTML += `Deseja Alugar? <a href="#alugueaqui"> Clique aqui!</a>`
     }
+}
+
+const obj_enviar_form = document.querySelector('#enviar_form')
+const obj_resposta = document.querySelector('#resposta')
+
+obj_enviar_form.addEventListener('click', FunEnviarDados)
+
+function FunEnviarDados(){
+    obj_resposta.innerHTML = 'Dados Enviados com Sucesso! Aguarde que Entraremos em contato com você.'
+
 }
